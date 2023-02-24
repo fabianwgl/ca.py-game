de1c3152ccccf4bcb3edfd87f70ee5e5c1e815af
Fabian General <fabianwgl@gmail.com>
2023-02-23 21:50:38 -0300

    feat: eat spawned seed - variable alpha channel

--- escene.py
+++ escene.py
@@ -11,7 +11,7 @@ mixer.music.load('lloyd.wav')
 mixer.music.set_volume(0.5)
 
 pygame.display.set_caption('capi the game')
-WINDOW_SIZE = (800, 400)
+WINDOW_SIZE = (900, 506)
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 display = pygame.Surface(WINDOW_SIZE)
 
@@ -42,6 +42,9 @@ bg5_image = pygame.image.load('clouds/5.png')
 single_cloud_image = pygame.image.load('clouds/3.png')
 single_cloud_image.set_alpha(230)
 
+# print(bg1_image.get_height())
+# print(bg1_image.get_width())
+
 CHUNK_SIZE = 8
 def generate_chunk(x,y):
     chunk_data = []
@@ -55,11 +58,9 @@ def generate_chunk(x,y):
             elif target_y == 10:
                 tile_type = 1 # grass
             elif target_y == 9:
-                if random.randint(1,99) == 1: # chance of spawning an oak seed
+                if random.randint(1,2) == 1: # chance of spawning an oak seed
                     tile_type = 3 # oak seed
             if tile_type != 0:
-                # print('ADD CHUNK DATA')
-                # print(target_x, target_y, tile_type)
                 chunk_data.append([[target_x, target_y], tile_type])
     return chunk_data
 
@@ -147,7 +148,7 @@ player_rect = pygame.Rect(200, 200, 64, 45)
 
 print("TILE SIZE "+str(TILE_SIZE*0.05))
 
-FACTOR_SCALE = 1.4
+FACTOR_SCALE = 1.6
 PLAYER_MOVEMENT_SPEED = 3
 
 #   draw background
@@ -160,14 +161,19 @@ image4 = pygame.transform.scale(bg4_image, (bg4_image.get_width()*FACTOR_SCALE, 
 image4_x = -20
 image4_return = False
 
-cloud = pygame.transform.scale(single_cloud_image, (single_cloud_image.get_width()*2, single_cloud_image.get_height()*2))
+cloud = pygame.transform.scale(single_cloud_image, (single_cloud_image.get_width(), single_cloud_image.get_height()))
 cloud_x = 0
 
 mixer.music.play()
 img3_x = 0
 clouds_x = 0
 clouds_y = 0
-main_text = "Si tengo apetito es solo..."
+main_text = "Oak seeds"
+seed_amount = 0
+alpha = 300
+
+last_oak_rect = pygame.Rect(0, 0, 1, 1)
+eaten_oaks = []
 
 main_font_french = pygame.font.SysFont('Verdana', 50)
 main_font = pygame.font.SysFont('Verdana', 20)
@@ -176,7 +182,7 @@ run = True
 while run:
     #   scroll and camera offset
     true_scroll[0] += (player_rect.x-true_scroll[0]-300)/5
-    true_scroll[1] += (player_rect.y-true_scroll[1]-290)/5
+    true_scroll[1] += (player_rect.y-true_scroll[1]-400)/5
     #   purify scroll integers 
     scroll = true_scroll.copy()
     scroll[0] = int(scroll[0])
@@ -185,19 +191,21 @@ while run:
     #   draw background
     display.blit(image, (0, 0))
     display.blit(image2, (0, 0))
-    display.blit(image3, (clouds_x, clouds_y))
+    image3.set_alpha(alpha)
+    display.blit(image3, (clouds_x, clouds_y-10))
     
     #   create and render background poem
-    main_text_render = main_font.render(main_text, True, (0,0,0))
+    main_text_render = main_font.render(f"{str(seed_amount)} {main_text}", True, (0,0,0))
     main_text_render.set_alpha(125)
-    display.blit(main_text_render, (200, 100))
+    display.blit(main_text_render, (100, 100))
 
 
-    display.blit(image5, (clouds_x, clouds_y))
+    display.blit(image5, (clouds_x, clouds_y-10))
 
     #   lullaby clouds animation
-    display.blit(cloud, (clouds_x, clouds_y))
-    display.blit(image4, (clouds_x*3, clouds_y*3))
+    # display.blit(cloud, (clouds_x, clouds_y))
+    image4.set_alpha(alpha)
+    display.blit(image4, (clouds_x*3, clouds_y*5))
     
     if not image4_return:
         image4_x += 1
@@ -212,37 +220,44 @@ while run:
 
 
     #   text poem logic
-    if player_rect.x < 500:
-        main_text = "Si tengo apetito es solo..."
+    # if player_rect.x < 500:
+    #     main_text = "Si tengo apetito es solo..."
         # letter = list(text).pop()
         # main_text += main_text + list(text).pop()
-    if player_rect.x > 500 and player_rect.x < 1000:
-        main_text = "...de la tierra y de las piedras"
-    if player_rect.x > 1000 and player_rect.x < 1500:
-        main_text = "Almuerzo siempre con aire..."
-    if player_rect.x > 1500 and player_rect.x < 2000:
-        main_text = "...tierra, carbones y piedras"
+    # if player_rect.x > 500 and player_rect.x < 1000:
+    #     main_text = "...de la tierra y de las piedras"
+    # if player_rect.x > 1000 and player_rect.x < 1500:
+    #     main_text = "Almuerzo siempre con aire..."
+    # if player_rect.x > 1500 and player_rect.x < 2000:
+    #     main_text = "...tierra, carbones y piedras"
 
 
 
     tile_rects = []
+    oak_seeds = []
     #   display tiles and things and 
     for y in range(3):
         for x in range(4):
             target_x = x - 1 + int(round(scroll[0]/(CHUNK_SIZE*TILE_SIZE_SCALE)))
             target_y = y - 1 + int(round(scroll[1]/(CHUNK_SIZE*TILE_SIZE_SCALE)))
             target_chunk = str(target_x) + ':' + str(target_y)
-            print(target_chunk)
             if target_chunk not in game_map:
                 game_map[target_chunk] = generate_chunk(target_x, target_y)
-                print(game_map[target_chunk])
             for tile in game_map[target_chunk]:
-                display.blit(tile_index[tile[1]], (tile[0][0]*TILE_SIZE_SCALE-scroll[0], tile[0][1]*TILE_SIZE_SCALE-scroll[1]))
                 if tile[1] in [1,2]:
+                    display.blit(tile_index[tile[1]], (tile[0][0]*TILE_SIZE_SCALE-scroll[0], tile[0][1]*TILE_SIZE_SCALE-scroll[1]))
                     tile_rects.append(pygame.Rect(tile[0][0]*TILE_SIZE_SCALE, tile[0][1]*TILE_SIZE_SCALE, TILE_SIZE_SCALE, TILE_SIZE_SCALE))
+                if tile[1] == 3:
+                    oak_rect = pygame.Rect(tile[0][0]*TILE_SIZE_SCALE, tile[0][1]*TILE_SIZE_SCALE, tile_index[tile[1]].get_width()/3, tile_index[tile[1]].get_height())
+                    oak_seeds.append(oak_rect)
+                    #   dont draw oak seeds that have been eaten
+                    # if not oak_rect == last_oak_rect:
+                    if oak_rect not in eaten_oaks:
+                        display.blit(tile_index[tile[1]], (tile[0][0]*TILE_SIZE_SCALE-scroll[0], tile[0][1]*TILE_SIZE_SCALE-scroll[1]+oak_seed_image.get_height()-5) )
 
 
-
+    # print('OAKSEEDS')
+    # print(oak_seeds)
 
     player_movement = [0,0]
     if moving_right and not moving_down:
@@ -252,44 +267,70 @@ while run:
         # clouds_x -= 0.03
         player_movement[0] -= PLAYER_MOVEMENT_SPEED
     if moving_up:
-        clouds_y += 0.03
+        clouds_y += 0.09
+        if alpha > -125:
+            alpha -= 1
         # print('moving up')
         # player_movement[1] += PLAYER_MOVEMENT_SPEED
     if moving_down:
         # print('moving down')
-        clouds_y -= 0.03
+        alpha += 1
+        clouds_y -= 0.09
         # player_movement[1] -= PLAYER_MOVEMENT_SPEED
         
+    # print(player_y_momentum)
 
     player_movement[1] += player_y_momentum
     player_y_momentum += velocity
 
+    #   player falling
+    if player_is_jumping and player_y_momentum > 0:
+        if alpha < 250:
+            alpha += 1
+        if clouds_y > 0:
+            clouds_y -= 1
+    #   don't fall way to fast
     if player_y_momentum > 10:
         player_y_momentum = 10
 
-    if player_movement[0] > 0:
+    #   player actions
+    if player_movement[0] > 0: #    walk right
         player_action, player_frame = change_action(player_action, player_frame, 'walk')
         img3_x += 0.01
         player_flip = False
-    if player_movement[0] < 0:
+    if player_movement[0] < 0: #    walk left
         player_action, player_frame = change_action(player_action, player_frame, 'walk')
         player_flip = True
-    #   IDLE
-    if player_movement[0] == 0:
+    if player_movement[0] == 0: #   idle
         player_action, player_frame = change_action(player_action, player_frame, 'sitting-idle')
-        # player_flip = False
-    if moving_down and not moving_left:
+    if moving_down and not moving_left: #   munch right
         player_action, player_frame = change_action(player_action, player_frame, 'munch')
-    if moving_down and moving_left:
+    if moving_down and moving_left: #   munch left
         player_action, player_frame = change_action(player_action, player_frame, 'munch')
         player_flip = True
 
 
     player_rect, collisions = move(player_rect, player_movement, tile_rects)
+    # player_rect2, collisions2 = move(player_rect, player_movement, oak_seeds)
+
+    #   check colission with oak seeds
+    oak_colission = player_rect.collideobjects(oak_seeds)
+    if player_rect.collideobjects(oak_seeds):
+        # print(oak_colission)
+        display.blit(oak_seed_image, (0,0))
+        if moving_down and oak_colission not in eaten_oaks:
+            seed_amount += 1
+            eaten_oaks.append(oak_colission)
+    # else:
+    #     print('not in oak')
+
     # print("player X "+str(player_rect.x))
     if collisions['bottom']:
         player_is_jumping = False
         player_y_momentum = 0
+    if collisions['right']:
+        print('COLLISSIONS RIGHT')
+        print(collisions)
 
     #   draw player sprite
     player_frame += 1
@@ -334,5 +375,5 @@ while run:
     # screen.blit(pygame.transform.scale(display, WINDOW_SIZE), (0,0))
     screen.blit(display, (0,0))
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(30)
     # print(clock.get_fps())
